e1e1e28ed9c3b8895b1bcb3ee0e4e0fdab8bb542
tahrer007 <tahrer_Ad@outlook.com>
2022-01-03 02:06:59 +0200

    2:06

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./game.css";
 import fillGameArray from "./intalizeTheGame";
+import Card from "./card/Card";
 
 class Game extends React.Component {
   state = {
     level: "",
     score: "",
     lives: 3,
-    numberOfCards: 24,
+    numberOfCards: 16,
     isWin: false,
     isLose: false,
     cardsArr: [],
@@ -25,7 +26,15 @@ class Game extends React.Component {
   render() {
     return (
       <div className="gameBoardContainer">
-        <div className="gameBoard" style={{gridTemplateColumns: `repeat(4, 1fr)`, gridTemplateRows: `repeat(4, 1fr)`}}></div>
+        <div className="gameBoard" style={{gridTemplateColumns: `repeat(4, 1fr)`, gridTemplateRows: `repeat(4, 1fr)`}}>
+        {this.state.cardsArr.map((element, index) => (
+            <Card
+              key={index}
+              image={element}
+              id={index}
+            />
+          ))}
+        </div>
         <div className="gameData"> game data</div>
       </div>
     );
--- src/components/Game/card/card.js
+++ src/components/Game/card/card.js
@@ -1,1 +1,25 @@
-import "./card.css"
+import React from "react";
+import "./card.css";
+import "./cardImages.css"
+
+
+const flipTheCard=(id,e,image)=>{
+
+  console.log(image);
+  e.target.classList.toggle("covered");
+  e.target.classList.toggle(`image${image}`);
+    
+}
+
+const Card = ({ id, image}) => {
+  
+  
+  return (
+    <div className="card backGround covered " onClick={(e)=>flipTheCard(id,e,image) }>
+        {image}
+      </div>
+  );
+};
+
+export default Card;
+
